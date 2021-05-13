a44c6602af00a44affff7cfbf0998cf564b766d1
zawy <46110056+zoeyalex@users.noreply.github.com>
2021-05-13 13:44:08 +0200

    add .config

--- install.sh
+++ install.sh
@@ -13,3 +13,7 @@ if diff -bq ~/.xinitrc $script_path/.xinitrc | grep -q "differ" ; then
 	echo -e "\n\033[0;31m.xinitrc differs"
 	cp -iv $script_path/.xinitrc ~
 fi
+if diff -bq ~/.config/picom.conf $script_path/.config/picom.conf | grep -q "differ" ; then
+	echo -e "\n\033[0;35mpicom.conf differs"
+	cp -iv $script_path/.config/picom.conf ~/.config
+fi
