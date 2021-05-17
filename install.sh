340df10598b84feecbd990f0570ab9dc27cbfa03
zawy <46110056+zoeyalex@users.noreply.github.com>
2021-05-18 00:30:36 +0200

    install.sh refactor

--- install.sh
+++ install.sh
@@ -1,23 +1,13 @@
 #!/bin/sh
+
 set -e
+
 script_path=$(dirname $(readlink -f $0))
-if  diff -bq ~/.bashrc $script_path/.bashrc | grep -q "differ" ; then
-	echo -e "\n\033[0;33m.bashrc differs"
-	cp -iv $script_path/.bashrc ~
-fi
-if  diff -bq ~/.vimrc $script_path/.vimrc | grep -q "differ" ; then
-	echo -e "\n\033[0;33m.vimrc differs"
-	cp -iv $script_path/.vimrc ~
-fi
-if diff -bq ~/.xinitrc $script_path/.xinitrc | grep -q "differ" ; then
-	echo -e "\n\033[0;33m.xinitrc differs"
-	cp -iv $script_path/.xinitrc ~
-fi
-if diff -bq ~/.config/picom.conf $script_path/.config/picom.conf | grep -q "differ" ; then
-	echo -e "\n\033[0;33mpicom.conf differs"
-	cp -iv $script_path/.config/picom.conf ~/.config
-fi
-if diff -bq ~/.config/dunst/dunstrc $script_path/.config/dunst/dunstrc | grep -q "differ" ; then
-	echo -e "\n\033[0;33dunstrc differs"
-	cp -iv $script_path/.config/dunst/dunstrc ~/.config/dunst/dunstrc
-fi
+
+cd $script_path/home
+for file in $(find -type f); do
+    if ! diff -q $file ~/$file >/dev/null; then
+        echo -e '\n\033[0;33m'$file differs
+        cp -iv $file ~/$file
+    fi
+done
