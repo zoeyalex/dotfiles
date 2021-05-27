c16fe326bc2fc24f7f440944310a73c6569b64c9
zawy <46110056+zoeyalex@users.noreply.github.com>
2021-05-27 20:11:18 +0200

    update scripts

--- home/.local/bin/status/modules/weather.sh
+++ home/.local/bin/status/modules/weather.sh
@@ -1,5 +1,4 @@
-    LOCATION=Waterloo,Belgium
-    SEP1="["
-    SEP2="]"
-    DATA=$(curl -s wttr.in/$LOCATION?format=1 | grep -o ".[0-9].*")
-    xsetroot -name "${SEP1}${DATA}${SEP2}"
+#!/bin/bash
+SEP1="["
+SEP2="]"
+WEATHER=$(curl -s wttr.in/?format=1 | grep -o ".[0-9].*")
--- home/.local/bin/status/status.sh
+++ home/.local/bin/status/status.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
-#
-set -e
 
-. ./.local/bin/volume.sh
-. ./.local/bin/weather.sh
+set -x
+
+SCRIPT_PATH=$(dirname $(readlink -f $0))
+
+. $SCRIPT_PATH/modules/volume.sh
+. $SCRIPT_PATH/modules/weather.sh
 
 TIME=`date +"%A %d-%m-%Y %R"`
 
