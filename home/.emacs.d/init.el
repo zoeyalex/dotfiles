b181acc1f7c2ba49a4bc4659598aefdb9906c082
zawy <46110056+zoeyalex@users.noreply.github.com>
2021-06-03 23:21:07 +0200

    udpate config

--- home/.emacs.d/init.el
+++ home/.emacs.d/init.el
@@ -1,5 +1,5 @@
+;; Disable junk
 (setq inhibit-startup-message t)
-
 (scroll-bar-mode -1)        ; Disable visible scrollbar
 (tool-bar-mode -1)          ; Disable the toolbar
 (tooltip-mode -1)           ; Disable tooltips
@@ -9,15 +9,15 @@
 ;; Set up the visible bell
 (setq visible-bell t)
 
-(set-face-attribute 'default nil :font "SourceCodePro-10")
-
-(load-theme 'dracula t)
+;; Change default font, theme
+(set-face-attribute 'default nil :font "Tamzen-11")
+(load-theme 'space t)
 
 
 ;; Make ESC quit prompts
 (global-set-key (kbd "<escape>") 'keyboard-escape-quit)
 
-;; Initialize package sources
+;; Initialize package manager
 (require 'package)
 (setq package-archives '(("melpa" . "https://melpa.org/packages/")
                          ("org" . "https://orgmode.org/elpa/")
@@ -33,11 +33,21 @@
 (require 'use-package)
 (setq use-package-always-ensure t)
 
-(use-package all-the-icons)
+;; Enable line numbers
+(column-number-mode)
+(global-display-line-numbers-mode t)
+;; Disable line numbers for some modes
+(dolist (mode '(org-mode-hook
+		term-mode-hook
+		eshell-mode-hook))
+  (add-hook mode (lambda () (display-line-numbers-mode 0))))
 
+
+;; Discord integratin
 (require 'elcord)
 (elcord-mode)
 
+;; Set up ivy/counsel and bind fuzzy find globally
 (use-package ivy
   :diminish
   :bind (("C-s" . swiper)
@@ -58,12 +68,20 @@
 (global-set-key (kbd "M-x") 'counsel-M-x)
 (global-set-key (kbd "C-x C-f") 'counsel-find-file)
 
-;; Customize modeline
+;; Customize modeline and provide icon fonts
+(use-package all-the-icons)
 (use-package doom-modeline
   :ensure t
   :init (doom-modeline-mode 1)
   :custom ((doom-modeline-height 15)))
 
+;; Package that makes creating themes easier
+(use-package autothemer
+  :ensure t)
+
+;; highligh hex, string colors etc.
+(use-package rainbow-mode
+  :hook (prog-mode . rainbow-mode ))
 
 ;; Use .custom.el for custom-set-variables
 (setq custom-file "~/.emacs.d/custom.el")
