6be884758deb587fe276ce9f19552b86b7631f06
zawy <46110056+zoeyalex@users.noreply.github.com>
2021-06-04 18:57:54 +0200

    udpate

--- home/.emacs.d/custom.el
+++ home/.emacs.d/custom.el
@@ -3,10 +3,10 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(inhibit-startup-screen t)
- '(initial-buffer-choice "/home/zoey/Pictures/uwu.png")
+ '(custom-safe-themes
+   '("63412740fd0ef0fe8807be60e3795c9a29544a867baf3092e8171c70f8ce14c5" default))
  '(package-selected-packages
-   '(emojify dashboard ivy-rich which-key rainbow-mode autothemer doom-modeline all-the-icons use-package shrink-path elcord dracula-theme counsel)))
+   '(helpful evil-collection undo-tree evil rainbow-delimiters general emojify dashboard ivy-rich which-key rainbow-mode autothemer doom-modeline all-the-icons use-package shrink-path elcord dracula-theme counsel)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
