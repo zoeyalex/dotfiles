84601e9b3a4714b2bc5faa3298603df7f341f8ec
zawy <46110056+zoeyalex@users.noreply.github.com>
2021-06-05 16:52:09 +0200

    udpate

--- home/.emacs.d/custom.el
+++ home/.emacs.d/custom.el
@@ -6,7 +6,7 @@
  '(custom-safe-themes
    '("63412740fd0ef0fe8807be60e3795c9a29544a867baf3092e8171c70f8ce14c5" default))
  '(package-selected-packages
-   '(hydra helpful evil-collection undo-tree evil rainbow-delimiters general emojify dashboard ivy-rich which-key rainbow-mode autothemer doom-modeline all-the-icons use-package shrink-path elcord dracula-theme counsel)))
+   '(projectile consult-lsp hydra helpful evil-collection undo-tree evil rainbow-delimiters general emojify dashboard ivy-rich which-key rainbow-mode autothemer doom-modeline all-the-icons use-package shrink-path elcord dracula-theme counsel)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
